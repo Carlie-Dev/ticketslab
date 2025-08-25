835072354a3f16a5088f87f53071e524066f04eb
Carlie-Dev <ellepheonixgames@gmail.com>
2025-08-25 15:20:26 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+define('TICKET_PRICE', 12.50);
+define('THEATER_NAME', 'OTC Cinema');
+
+$customer = "Brian Van Halbert";
+$tickets = "2";
+$hasCoupon = true;
+$snack = null;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tickets Lab</title>
+</head>
+<body>
+    <h1></h1>
+</body>
+</html>
