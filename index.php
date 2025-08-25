5e7427206acd5666826dba00509d0f9e77ee0011
Carlie-Dev <ellepheonixgames@gmail.com>
2025-08-25 17:06:02 -0500

    decorated heading

--- index.php
+++ index.php
@@ -16,29 +16,37 @@ $finaltotal;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tickets Lab</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    <h1><?php echo THEATER_NAME?></h1>
-    <p><?php echo "Name: " . $customer ?></p> <!-- concatination -->
-    <p><?php echo "Ticket(s): $tickets" ?></p> <!-- interpolation -->
+<body class="p-3 mb-2 bg-dark text-white">
+    <div class = "border border-white border-5 d-flex d-flex justify-content-center shadow p-3 mb-5 rounded">
+        <h1><?php echo THEATER_NAME?></h1>
+    </div>
+    
+   
+    <div>
+        <p><?php echo "Name: " . $customer ?></p> <!-- concatination -->
+        <p><?php echo "Ticket(s): $tickets" ?></p> <!-- interpolation -->
 
     <!--casting --> 
-    <p>Type - $tickets: <?php echo gettype($tickets)?></p>
-    <?php $tickets=(int)$tickets?>
-    <p>New Type - $tickets: <?php echo gettype($tickets)?></p>
+        <p>Type - $tickets: <?php echo gettype($tickets)?></p>
+        <?php $tickets=(int)$tickets?>
+        <p>New Type - $tickets: <?php echo gettype($tickets)?></p>
 
     <!-- price calculation-->
-    <p><?php
-        $subtotal = $tickets*TICKET_PRICE;
-        $finaltotal = $subtotal;
-        if($hasCoupon){
-            $finaltotal = $subtotal - 2.00;
-            echo "YAY! Discount applied!";
+        <p><?php
+            $subtotal = $tickets*TICKET_PRICE;
+            $finaltotal = $subtotal;
+            if($hasCoupon){
+                $finaltotal = $subtotal - 2.00;
+                echo "YAY! Discount applied!";
 
-        }else{
-            echo "No Discounts Applied";
-        }
-    ?></p>
+            }else{
+                echo "No Discounts Applied";
+            }
+        ?></p>
+    </div>
+
     <table>
         <tr>
             <th>Subtotal: <th>
